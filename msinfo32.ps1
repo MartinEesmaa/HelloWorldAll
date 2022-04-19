4dc3a2b1a1149d5598baeb307fb84cf579b0d6de
Martin Eesmaa <88035011+MartinEesmaa@users.noreply.github.com>
2022-04-19 23:51:22 +1000

    fixed to remove codes of dollar signs

--- msinfo32.ps1
+++ msinfo32.ps1
@@ -1,14 +1,5 @@
-$System = Get-CimInstance CIM_ComputerSystem
-$BIOS = Get-CimInstance CIM_BIOSElement
-$OS = Get-CimInstance CIM_OperatingSystem
-$CPU = Get-CimInstance CIM_Processor
-$HDD = Get-CimInstance Win32_LogicalDisk -Filter "DeviceID = 'C:'"
-$EXTXT = "$env:USERPROFILE\Desktop\welp.txt"
-Clear-Host
-
-"Manufacturer: " + $System.Manufacturer >> $EXTXT
-"Model: " + $System.Model >> $EXTXT
-"CPU: " + $CPU.Name >> $EXTXT
-"RAM: " + "{0:N2}" -f ($System.TotalPhysicalMemory/1GB) + "GB" >> $EXTXT
-"HDD Capacity: "  + "{0:N2}" -f ($HDD.Size/1GB) + "GB" >> $EXTXT
-"Operating System: " + $OS.caption >> $EXTXT
+Get-CimInstance CIM_ComputerSystem
+Get-CimInstance CIM_BIOSElement
+Get-CimInstance CIM_OperatingSystem
+Get-CimInstance CIM_Processor
+Get-CimInstance Win32_LogicalDisk -Filter "DeviceID = 'C:'"
